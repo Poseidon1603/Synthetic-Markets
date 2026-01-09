ceb690c7f2e1b54acc6d653d5d83094d481802c9
Poseidon1603 <ntheo999@gmail.com>
2026-01-09 12:44:53 +0100

    added checking for increment mean and put logging in its own folder (basically the data outputted was like 200MB and didnt want to bomb github with it

--- Random-Walk/random-walk.cpp
+++ Random-Walk/random-walk.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <random>
 #include <fstream>
+#include <vector>
 using namespace std;
+
+double check_increment_mean(fstream f) {
+    vector<double> sum;
+    string temp;
+    // This puts all the increment values into the vector sum
+    while (getline(f,temp)) {
+        temp = temp.substr(temp.find(","));
+        sum.push_back(stod(temp));
+    }
+
+    sum.push_back(0);
+    // This is the sum of increments
+    for (double val : sum) {
+        sum.back() += val;
+    }
+
+    // Returns the mean of increments
+    return sum.back() / (sum.size() - 1);
+}
 
 double random_sample(double mean = 0, double stdev = 0.5) {
     static mt19937 rng{random_device{}()};
@@ -18,7 +38,7 @@ double pdf(auto x = 5, auto mean = 0, auto stdev = 1) {
 }
 
 int main(int argc, const char *argv[]) {
-    ofstream file("random-walk-log.csv", ios::app);
+    fstream file("log/random-walk-log.csv", ios::app);
     double price = 0;
     if (!argc || argc == 1) {
         cout << "Give me how long you want data for pls" << endl;
@@ -29,10 +49,14 @@ int main(int argc, const char *argv[]) {
        duration *= (24 * 60 * 60);
        // for every second we want to generate some data from the pdf function
        for (int i = 0; i < duration; i++) {
-        price += random_sample(0,0.08);
-        file << price << endl;
-        cout << "Current Price: " << price << endl;
+        double incr_value = random_sample(0,0.08);
+        price += incr_value;
+        file << price << "," << incr_value << endl;
        }
+
+       // Check the mean of the increments
+
+       file.close();
     } else {
         cout << "Too many arguments chief" << endl;
     }
